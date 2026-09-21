086048621aec67f7f5dab1b4a1f23573e0c26415
Casper <caspervgameren@outlook.com>
2026-09-21 12:49:57 +0200

    REMOVED other files

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,2 @@
 import './styles/tailwind.css'
 import './styles/main.scss'
-import heroImg from './assets/hero.png'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
