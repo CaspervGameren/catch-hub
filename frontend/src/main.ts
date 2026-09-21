0b53668e2890513ae7c18f98e8ced686a345b965
Casper <caspervgameren@outlook.com>
2026-09-21 11:33:19 +0200

    ADDED pages setup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
-import './style.css'
+import './styles/tailwind.css'
+import './styles/main.scss'
 import heroImg from './assets/hero.png'
 import typescriptLogo from './assets/typescript.svg'
 import viteLogo from './assets/vite.svg'
